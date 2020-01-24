bcca3ad93fbad62ba585e981ae87d29b5933f209
Haithem Saoudi <haithem@staffbase.com>
2020-01-24 14:53:25 +0100

    :poop: Conversion from Roman Numerals to Numbers Implemented & Tested

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
+enum class Character {
+    I, V, X, L, C, D, M
+}
+fun romanToNumber(number: String){
+    
+}
+/*fun convertToNumbers(number:String):Int {
+    when (number) {
+        "I" -> return 1
+        "II" -> return 2
+        "III" -> return 3
+        "IV" -> return 4
+        "V" -> return 5
+        "VI" -> return 6
+        "VII" -> return 7
+        "VIII" -> return 8
+        "IX" -> return 9
+        "X" -> return 10
+        else -> { // Note the block
+            print("Invalid input")
+        }
+    }
+    return 0
+}*/
+
 fun main() {
     println("Test!")
 }
---
+++ src/test/kotlin/com/staffbase/example/MainKtTest.kt
@@ -0,0 +1,17 @@
+package com.staffbase.example
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class MainKtTest {
+    @Test
+    fun `can convert roman numerals to numbers`(){
+
+    }
+
+    @Test
+    private fun convertToNumbers() {
+
+
+    }
+}
