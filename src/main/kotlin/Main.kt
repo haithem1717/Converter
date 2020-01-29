34fd1c9868fa82f8ced727d4b1f7adc57dec50da
Haithem Saoudi <haithem@staffbase.com>
2020-01-29 17:45:18 +0100

    Cleaned Code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,9 +27,29 @@ fun String.parseAsRomanNumeral(): Int {
         .value
 }
 
+enum class Character(val value: Int) {
+    I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400), D(500), CM(900), M(1000)
+}
+
+data class Roman(var asString: String, var asInteger: Int)
+
+fun parseIntToRoman(number: Int): Roman {
+    val numeral = Roman("", 0)
+    val listOfCategories = listOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1).filter { it <= number }
+    listOfCategories.forEach { category ->
+        while (numeral.asInteger <= number - category)
+            numeral.apply{
+                numeral.asInteger += category
+                numeral.asString +=  Character.values().firstOrNull { it.value == category }!!
+            }
+    }
+    return numeral
+}
+
 fun main() {
-    sequenceOf(
+    /*sequenceOf(
         "CMXXVIII", "DCCCXXXV", "MMDCLXXXII", "CVIII", "MMMCMXCIX",
         "MDCCCXXVIII", "CDXXXVIII"
-    ).map(String::parseAsRomanNumeral).forEach(::println)
+    ).map(String::parseAsRomanNumeral).forEach(::println)*/
+    println(parseIntToRoman(1234).asString)
 }
--- src/test/kotlin/com/staffbase/example/MainKtTest.kt
+++ src/test/kotlin/com/staffbase/example/MainKtTest.kt
@@ -1,9 +1,14 @@
 package com.staffbase.example
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import parseIntToRoman
 import parseAsRomanNumeral
+import kotlin.test.assertEquals
+
+fun parseAsDecimal(n: Int) = n.toString()
+fun String.parseAsRomanNumeral() = toInt()
 
 internal class MainKtTest {
     @ParameterizedTest(name = "{index} => input(roman): {1} expected(decimal): {0} ")
@@ -13,6 +18,18 @@ internal class MainKtTest {
         "3999, MMMCMXCIX"
     )
     fun `can convert to numbers`(decimal: Int, roman: String){
-        assertEquals(decimal, roman.parseAsRomanNumeral())
+        assertEquals(expected = decimal, actual = roman.parseAsRomanNumeral())
+    }
+
+    @ParameterizedTest( name = "{index} => input(decimal): {1} expected(roman): {0} ")
+    @CsvSource( "MMMCM, 3900")
+    fun `can convert to roman numerals`(roman : String, decimal: Int){
+        assertEquals(expected = roman, actual = parseIntToRoman(decimal))
+    }
+
+    @RepeatedTest(3998)
+    fun `forth and back should work`(index: Int) {
+        val number = index + 1
+        assertEquals(expected = number, actual = parseIntToRoman(number).parseAsRomanNumeral())
     }
 }
